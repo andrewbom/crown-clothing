f881f9e939691a218d2f4f47cbfe087fb6d8ab58
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-09-21 23:16:13 +1000

    added carticon and cartdropdown components & cartReducer

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -5,8 +5,10 @@ import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -27,9 +29,24 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
+
+// 'mapstatetoprops' is to get your STATE from the redux store || 'mapdispatchtoprops' is to get the ACTIONS.
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
+
+export default connect(mapStateToProps)(Header);
+
+// -------------------Original Code-------------------------
+// const mapStateToProps = (state) => ({
+//   currentUser: state.user.currentUser,
+// });
 
 // 'mapstatetoprops' is to get your STATE from the redux store || 'mapdispatchtoprops' is to get the ACTIONS.
 // "state" === the same value returned by a call to store.getState() due to connect(mapStateToProps)
@@ -37,13 +54,8 @@ const Header = ({ currentUser }) => (
 
 // the first "currentUser" is the variable we define here and pass it to redux
 // it then becomes the prop of <Header> component and get destructured at the top
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
-});
 
 // 1) state.user => is calling the userReducer from root_reducer
 // 2) userReducer is a function that returns a new object based on the action.type
 // 3) Since here we called the userReducer without any action.type, it runs the default case
 // 4) The default case returns the current state inside the userReducer ( either the initial state or the latest state after the last action was fired )
-
-export default connect(mapStateToProps)(Header);
