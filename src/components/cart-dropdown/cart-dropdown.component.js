cfa0786a5fd8db9f52abc6ae06dde78566bc9394
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-09-22 17:36:24 +1000

    created cartItem component and connected to cartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,18 +1,21 @@
 import React from "react";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 // import { createStructuredSelector } from "reselect";
 // import { withRouter } from "react-router-dom";
 
 import CustomButton from "../custom-button/custom-button.component";
-// import CartItem from "../cart-item/cart-item.component";
+import CartItem from "../cart-item/cart-item.component";
 // import { selectCartItems } from "../../redux/cart/cart.selectors";
 // import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
       {/* {cartItems.length ? (
         cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
@@ -36,5 +39,9 @@ const CartDropdown = () => (
 //   cartItems: selectCartItems,
 // });
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
 // export default withRouter(connect(mapStateToProps)(CartDropdown));
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
